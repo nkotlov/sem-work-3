f3561235d51932bdc6091280297609275bcd7c2b
nk <143865876+nkotlov@users.noreply.github.com>
2025-06-05 01:04:55 +0300

    Add files via upload

---
+++ MoviesbymoodApplication.java
@@ -0,0 +1,20 @@
+package com.example.moviesbymood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+public class MoviesbymoodApplication {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesbymoodApplication.class, args);
+    }
+}
